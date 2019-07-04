fd505495053492ae86dec6de7543decd2f5b4d86
pvervliet <philippe.vervliet@gmail.com>
2019-07-04 18:16:22 +0200

    Cleanup explanations

--- Form2Elements.R
+++ Form2Elements.R
@@ -1,11 +1,20 @@
-a<-read.table("Formulas.txt", sep="\t", header=T)
+### Packages ###
+################
+library(tidyverse)
 
+### Import Data ###
+###################
+Forms <- read.table("Formulas.txt", sep="\t", header=T)
+
+### Define function ###
+#######################
 mysplit<-function(s)
 {
 g<-strsplit(as.character(s), split="")[[1]]
 
-#searching for capital letters or non-capital letters followed by capital letters or end of the line
+#Search for capital letters or non-capital letters followed by capital letters or end of the line
 tmp<-unlist(gregexpr("([[:upper:]]|[[:lower:]])(?=[A-Z]|$)",s,perl=TRUE))
+
 #if anything found, insert number one after the first position found
 if(length(which(tmp>-1))>0){g[tmp]<-paste(g[tmp],1,sep="")}
 
@@ -21,23 +30,28 @@ names(nums)<-ls
 return(nums)
 }
 
-#lets apply our function on the columng with molecular formulas
-res<-lapply(as.character(a$MOLECULAR.FORMULA),function(x) mysplit(s=x))
+### Apply function on the column with molecular formulas ###
+############################################################
 
-#getting all chemicals
+res<-lapply(as.character(Forms$MOLECULAR.FORMULA),function(x) mysplit(s=x))
+
+#Get all chemicals
 allchem<-unlist(lapply(res,function(x) names(x)))
 
-#creating matrix
-mat<-matrix(0,ncol=length(table(allchem)), nrow=nrow(a))
+#Create matrix
+mat<-matrix(0,ncol=length(table(allchem)), nrow=nrow(Forms))
 colnames(mat)<-names(sort(table(allchem), decreasing=T))
 
-#filling matrix
+### Fill matrix ###
+###################
 for (i in c(1:length(res)))
 {
   mat[i,names(res[[i]])]<-as.numeric(res[[i]])
 }
 
-a[,2:c(ncol(mat)+1)]<-mat
-colnames(a)[2:c(ncol(mat)+1)]<-colnames(mat)
+Forms[,2:c(ncol(mat)+1)]<-mat
+colnames(Forms)[2:c(ncol(mat)+1)]<-colnames(mat)
 
-write.table(a,file="CompTox_list-dupl_rem-ACI_MATRIX.txt", row.names=FALSE, sep="\t")
+### Export ###
+##############
+write.table(Forms,file="CompTox_list-dupl_rem-ACI_MATRIX.txt", row.names=FALSE, sep="\t")
